cdfdd9ece57f15c3ce89be83e4bb5fd206a23bf1
shalom ogoziem <shalomzymike@gmail.com>
2024-01-16 12:13:05 +0100

    testing time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
-"""defines a """
+"""defines a coroutine measure_comprehension"""
 import time
 import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    start_time = time.time()
+    """
+    Import async_generator from the previous task and then
+    write a coroutine called async_comprehension that takes
+    no arguments.
+    The coroutine will collect 10 random numbers using
+    an async comprehensing over async_generator,
+    then return the 10 random numbers.
+    """
+    start_time = time.perf_counter()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
     )
-    end_time = time.time()
+    end_time = time.perf_counter()
 
     return end_time - start_time
