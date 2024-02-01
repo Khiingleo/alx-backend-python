cef6817d751a70f15e286564f0c3edba247569ce
shalom ogoziem <shalomzymike@gmail.com>
2024-02-01 14:52:46 +0100

    test exception handling

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,12 +26,12 @@ class TestAccessNestedMap(unittest.TestCase):
 
     # test cases for exception handling
     @parameterized.expand([
-        ({}, ("a",), "KeyError: 'a'"),
-        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+        ({}, ("a",), KeyError, "'a'"),
+        ({"a": 1}, ("a", "b"), KeyError, "'b'"),
     ])
     def test_access_nested_map_exception(self, nested_map, path,
-                                         expected_exception_message):
-        """ test method for raising correct exception """
-        with self.assertRaises(KeyError) as context:
+                                         exception, expected_except_message):
+        """ test for exceptions in the access_nested_map """
+        with self.assertRaises(exception) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_exception_message)
+        self.assertIn(expected_except_message, str(context.exception))
